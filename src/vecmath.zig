10ee14d6b6808ed3800f73bc34f12c4c168c5f33
Jake Keast <JRKeast2003@outlook.com>
2021-07-26 15:59:30 +0100

    wrote small vector maths library

---
+++ src/vecmath.zig
@@ -0,0 +1,41 @@
+const std = @import("std");
+const vec3 = std.meta.Vector(3, f64);
+
+const abs = std.math.absFloat;
+const min = std.math.min;
+
+const Prng = std.rand.DefaultPrng;
+const pi: f64 = 3.1415926535897932385;
+
+pub fn magnitude(v: vec3) f64 {
+    return @sqrt(magnitude_squared(v));
+}
+
+pub fn magnitude_squared(v: vec3) f64 {
+    return v[0] * v[0] + v[1] * v[1] + v[2] * v[2];
+}
+
+pub fn dot(u: vec3, v: vec3) f64 {
+    return u[0] * v[0] + u[1] * v[1] + u[2] * v[2];
+}
+
+pub fn cross(u: vec3, v: vec3) vec3 {
+    return .{
+        (u[1] * v[2] - u[2] * v[1]),
+        (u[2] * v[0] - u[0] * v[2]),
+        (u[0] * v[1] - u[1] * v[0]),
+    };
+}
+
+pub fn normalise(v: vec3) vec3 {
+    return v / @splat(3, magnitude(v));
+}
+
+pub fn random(s: u64) vec3 {
+    var seed = rand.DefaultPrng.init(s);
+    return vec3{ seed.random.float(f64), seed.random.float(f64), seed.random.float(f64) };
+}
+
+pub fn degrees_to_radians(angle: f64) f64 {
+    return angle * (pi / 180.0);
+}
