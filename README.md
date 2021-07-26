a4c7396048f4692a5574ed3bd9aa6d89817f9966
Jake Keast <JRKeast2003@outlook.com>
2021-07-26 15:38:57 +0100

    stdout writer and main skeleton

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # pyxis
 Pyxis is a CPU Render Engine that utilises Monte Carlo Ray Tracing, written in Zig. This was a just for fun project I took on in order to build an understanding of the Zig language and to learn about ray tracing.
+The program is invoked by "zig run src/main.zig | bzip2 > <filename>.ppm.bz2". Its clunky, but the ppm file format was the easiest to work with.
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,48 @@
 const std = @import("std");
+const ArrayList = std.ArrayList;
+const rand = std.rand;
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+// Defines the vector as 3 floating point values in 3D space, and aliases point3 and color as vec3 for readability.
+const vec3 = std.meta.Vector(3, f64);
+const point3 = vec3;
+const color = vec3;
+
+// Memory Allocation
+var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+var allocator = &arena.allocator;
+
+// Image Data
+const aspect_ratio: f64 = 16.0 / 9.0;
+const height: u64 = 255;
+const width: u64 = @floatToInt(u64, @intToFloat(f64, height) * aspect_ratio); // Calculate width from height and aspect ratio (height * aspect ratio)
+
+// Writer
+const writer = std.io.getStdOut().writer();
+
+pub fn main() !void {
+    // When this function exits, call arena.deinit()
+    defer arena.deinit();
+    // Write PPM header
+    try writer.print("P3\n{} {}\n255\n", .{ width, height });
+
+    var j: i64 = height - 1;
+    while (j >= 0) : (j -= 1) {
+        var i: i64 = 0;
+        while (i < width) : (i += 1) {
+            var r: f64 = 1.0;
+            var g: f64 = 0.0;
+            var b: f64 = 0.0;
+            var c: color = .{ r, g, b };
+            try write_pixel(c);
+        }
+    }
+}
+
+// Write color data for each pixel to standard output
+fn write_pixel(c: color) !void {
+    try writer.print("{} {} {}\n", .{
+        @floatToInt(i64, 255.999 * @sqrt(c[0])),
+        @floatToInt(i64, 255.999 * @sqrt(c[1])),
+        @floatToInt(i64, 255.999 * @sqrt(c[2])),
+    });
 }
